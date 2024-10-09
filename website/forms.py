01d7b770f0824ecb1e5d31fc75d6b9f4081b6678
Andres Oliveira <andresoliveira@protonmail.com>
2024-10-08 23:18:41 -0300

    add: signup + form validation

--- website/forms.py
+++ website/forms.py
@@ -34,15 +34,6 @@ class SignUpForm(UserCreationForm):
         )
     )
 
-    #### Username ####
-    user_name = forms.CharField(
-        label="",
-        max_length="50",
-        widget=forms.TextInput(
-            attrs={'class':'form-control', 'placeholder': 'Username'}
-        )
-    )
-
     class Meta:
         model = User
         fields = (
